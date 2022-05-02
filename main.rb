8dd8582fe50e96dde659d9f0bcb54c8b391dc93d
Samuel ODaniels <samodan195@gmail.com>
2022-05-02 23:45:05 +0100

    Remove unnecessary variable and use value directly

--- main.rb
+++ main.rb
@@ -1,7 +1,6 @@
 def substring(string, array)
   result_arr = []
-  strray = string.split
-  strray.each do |str|
+  string.split.each do |str|
     result_arr << array.find_all { |word| str.include?(word) }
   end
   result_arr.flatten!.reduce(Hash.new(0)) do |result, item|
