fc65feafe5c0dd8980f08aabe33021db933c3ed1
Samuel ODaniels <samodan195@gmail.com>
2022-05-03 00:10:17 +0100

    Make program find more results by ignoring input case

--- main.rb
+++ main.rb
@@ -1,7 +1,7 @@
 def substring(string, array)
   result_arr = []
   string.split.each do |str|
-    result_arr << array.find_all { |word| str.include?(word) }
+    result_arr << array.find_all { |word| str.downcase.include?(word.downcase) }
   end
   result_arr.flatten!.reduce(Hash.new(0)) do |result, item| #add each new item to a hash with a default value of 0 incremented by 1 each time the same item is added
     result[item] += 1
